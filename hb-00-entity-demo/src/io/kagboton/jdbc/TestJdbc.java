dfe8478d0d34e371ac1850f9d2117a54fc1ecfdc
Kilian Agboton <kilian.agboton@gmail.com>
2021-03-16 23:13:47 -0400

    added mysql db scripts to hb-00-entity-demo

--- hb-00-entity-demo/src/io/kagboton/jdbc/TestJdbc.java
+++ hb-00-entity-demo/src/io/kagboton/jdbc/TestJdbc.java
@@ -8,8 +8,8 @@ public class TestJdbc {
     public static void main(String[] args) {
 
         String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
-        String user = "root";
-        String pwd = "root";
+        String user = "hb_student";
+        String pwd = "hb_student";
 
         try{
             System.out.println("Connection to database: " + jdbcUrl);
