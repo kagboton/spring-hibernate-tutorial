74908f00d344c35c461a527ec4cfc0c94c2762bd
Kilian Agboton <kilian.agboton@gmail.com>
2021-03-16 23:15:14 -0400

    hibernate :: update one-to-one and one-to-many modules

--- hb-02-one-to-many/src/io/kagboton/jdbc/TestJdbc.java
+++ hb-02-one-to-many/src/io/kagboton/jdbc/TestJdbc.java
@@ -7,7 +7,7 @@ public class TestJdbc {
 
     public static void main(String[] args) {
 
-        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-02-one-to-many-bi?useSSL=false";
         String user = "root";
         String pwd = "root";
 
